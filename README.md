bd28584f7c1f538e18080447e6bd6e64df1f5efd
netanelravid <nby018@gmail.com>
2017-02-19 15:36:13 +0200

    Travis CI, Landscape code health , Coverall code coverage , Requires.io packages dependencies & badges links

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # screener
-Screenshot any target webapp --> fast!!
+[![Travis](https://img.shields.io/travis/netanelravid/screener.svg)](https://travis-ci.org/netanelravid/screener)
+[![Code Health](https://landscape.io/github/netanelravid/screener/master/landscape.svg?style=flat)](https://landscape.io/github/netanelravid/screener)
+[![Python Version](https://img.shields.io/badge/python-v2.7-brightgreen.svg)](https://github.com/netanelravid/screener/)
+[![Coverage Status](https://coveralls.io/repos/github/netanelravid/screener/badge.svg?branch=master)](https://coveralls.io/github/netanelravid/screener)
+[![Requirements Status](https://requires.io/github/netanelravid/screener/requirements.svg)](https://requires.io/github/netanelravid/screener/requirements/?branch=master)
+
+Screenshot any target webapp --> fastest!!
--- tests/unit/test_utils_http_client.py
+++ tests/unit/test_utils_http_client.py
@@ -7,19 +7,16 @@ from screener.utils.images import IMAGE_EXT
 from tests.helpers import get_resource
 
 
-@pytest.fixture(scope='function')
-def browser():
-    return Browser()
-
-
-def test_take_screenshot(browser, httpserver, tmpdir):
+def test_take_screenshot(httpserver, tmpdir):
     folder = tmpdir.mkdir('screenshots').strpath
     filename = 'test_screenshot'
     page_source = get_resource('example_com.html')
     httpserver.serve_content(page_source)
-    browser.take_screenshot(url=httpserver.url,
-                            folder=folder,
-                            filename=filename)
+
+    with Browser() as browser:
+        browser.take_screenshot(url=httpserver.url,
+                                folder=folder,
+                                filename=filename)
     screenshot_filename = '{fname}.{ext}'.format(fname=filename, ext=IMAGE_EXT)
     file_path = path.join(folder, screenshot_filename)
     assert path.isfile(file_path)
